27373209af13cfb1db876036998587d911115f1c
topStud <topaz083@gmail.com>
2019-12-25 18:26:31 +0200

    changed

--- Client.cpp
+++ Client.cpp
@@ -3,16 +3,18 @@
 //
 
 #include "Client.h"
+#include "UpdateSimulator.h"
+#include "SymbolTable.h"
 /*
  * creates socket and returns file descriptor
  */
 int Client::create_socket(){
-  int client_socket = socket(AF_INET, SOCK_STREAM,0);
-  if (client_socket == -1) {
+  int my_socket = socket(AF_INET, SOCK_STREAM,0);
+  if (my_socket == -1) {
     // error
     throw "Could not create a socket";
   }
-  return client_socket;
+  return my_socket;
 }
 
 /*
@@ -20,15 +22,16 @@ int Client::create_socket(){
  * creates sockaddr object
  * requests a connection with the server on local host with port 5402
  */
-void Client::connect_com(const std::string& ip, int port) const{
+void Client::connect_com(const std::string& ip, int port) const {
   // create sockaddr obj
-  sockaddr_in address;
+  sockaddr_in address{};
   address.sin_family = AF_INET;
   address.sin_addr.s_addr = inet_addr(ip.c_str());
   address.sin_port = htons(port);
 
   // connect command
-  if (connect(client_socket, (struct sockaddr*) &address, sizeof(address)) == -1) {
+  int is_connect = connect(client_socket, (struct sockaddr*) &address, sizeof(address));
+  if (is_connect == -1) {
     throw "Could not connect to simulator";
   } else {
     std::cout << "Client is now connected to the server" << std::endl;
@@ -44,28 +47,36 @@ void Client::send_message_to_server(std::string str) const {
   }
 }
 
-int Client::get_client_sock() const {
-  return client_socket;
-}
-
 void Client::close_socket() const{
   close(client_socket);
 }
 
 Client::Client(const std::string& ip, int port) {
-  flag_stop_communication = false;
+  flag_stop_communication_client = false;
   client_socket = create_socket();
   connect_com(ip, port);
 }
 
 void start_sock(const Client& c) {
-  while (true) {
+  std::string key{}, message{};
+  double value;
 
+  while (true) {
     // check if the communication continues
-    if (flag_stop_communication) {
+    if (flag_stop_communication_client) {
       c.close_socket();
       break;
     }
 
+    if (UpdateSimulator::get_instance()->updates_available()) {
+      key = UpdateSimulator::get_instance()->pop_update();
+      if (SymbolTable::get_instance()->key_exists_server_map(key)) {
+        value = SymbolTable::get_instance()->get_key_value_server(key).get_value();
+        message = "set " + SymbolTable::get_instance()->get_key_value_server(key).get_sim() + " " + std::to_string(value) + "/r/n";
+        c.send_message_to_server(message);
+      } else {
+        std::cout << "an error from the client, the key was not recognized in the map";
+      }
+    }
   }
 }
